939427f8d9ec9624a88d88e64fcd8e049e2b9819
sikeacab-dot <sikeacab@gmail.com>
2026-05-28 05:03:10 +0300

    Add files via upload

--- products.js
+++ products.js
@@ -1,5 +1,255 @@
 var products = [
 {
+"id": 1779933225171,
+"name": "Шу Пуер 908",
+"category": "Puerh",
+"price": 550,
+"variants": {
+"50": 300,
+"100": 550,
+"200": 1300,
+"250": 1300
+},
+"image": "assets/products/shu9081.webp",
+"images": [
+"assets/products/shu9081.webp",
+"assets/products/shu9082.webp",
+"assets/products/shu9083.webp"
+],
+"origin": "Юнань, Китай",
+"description": "Відомий 908 рецепт приготування пуеру від фабрики Haiwan, дуже зручний варіант щоб завжди мати з собою порцію улюбленого чаю. Також ідеально підійде до знайомства з пуерами.",
+"on_order": false,
+"badge": "NEW",
+"brewing": {
+"steeps": 0,
+"time": 20,
+"grams": 0
+}
+},
+{
+"id": 1779931542738,
+"name": "Шен Пуер \"Точа\"",
+"category": "Puerh",
+"price": 400,
+"variants": {
+"50": 250,
+"100": 400
+},
+"image": "assets/products/shengtuo5.webp",
+"images": [
+"assets/products/shengtuo5.webp",
+"assets/products/shengtuo1.webp",
+"assets/products/shengtuo2.webp",
+"assets/products/shengtuo3.webp",
+"assets/products/shengtuo4.webp"
+],
+"origin": "Юнань, Китай",
+"description": "Класичний Шен пуер, котрий дарує бодрість, концентрацію та прекрасно доповнює будь який момент. Заварювати бажано при 90-95 градусах, та проливами не більше 5 секунд.",
+"on_order": false,
+"badge": "NEW",
+"brewing": {
+"steeps": 0,
+"time": 20,
+"grams": 0
+}
+},
+{
+"id": 1775867642449,
+"name": "Габа Да Хун Пао",
+"category": "Gaba",
+"price": 600,
+"variants": {
+"50": 350,
+"100": 600
+},
+"image": "assets/products/GabaDPH2.webp",
+"images": [
+"assets/products/GabaDPH2.webp",
+"assets/products/GabaDHP1.webp"
+],
+"origin": "Китай, провінція Фуцзянь, гори Уї",
+"description": "Унікальне поєднання скельного характеру Да Хун Пао та заспокійливої дії Габа-чаю. У смаку відчуваються пряні нотки вогню, печеної корочки хліба та легка ягідна кислинка. Цей чай м’яко \"перезавантажує\" нервову систему, знімаючи напругу після складного дня, при цьому залишаючи розум ясним.",
+"on_order": false,
+"badge": "HIT",
+"brewing": {
+"steeps": 5,
+"time": 20,
+"grams": 8
+}
+},
+{
+"id": 1779932529362,
+"name": "Шу Пуер Гунтін \"Зірки\" ",
+"category": "Puerh",
+"price": 370,
+"variants": {
+"50": 250,
+"100": 370,
+"357": 1300
+},
+"image": "assets/products/shustars.webp",
+"images": [
+"assets/products/shustars.webp",
+"assets/products/shustars4.webp",
+"assets/products/shustars3.webp",
+"assets/products/shustars2.webp"
+],
+"origin": "Юнань, Китай",
+"description": "Чайний лист, зібраний з старих дерев в 2013 році, котрий пройшов ферментацію та отримав дуже легкий, незвичний для пуеру смак. Для поціновачів витриманих пуерів з великою кількістю бруньок. ",
+"on_order": false,
+"badge": "NEW",
+"brewing": {
+"steeps": 0,
+"time": 20,
+"grams": 0
+}
+},
+{
+"id": 1779931949990,
+"name": "Шу Пуер \"7663\" Точа",
+"category": "Puerh",
+"price": 500,
+"variants": {
+"50": 300,
+"100": 500
+},
+"image": "assets/products/shu7663tuo1.webp",
+"images": [
+"assets/products/shu7663tuo1.webp",
+"assets/products/shu7663tuo2.webp",
+"assets/products/shu7663tuo3.webp",
+"assets/products/shu7663tuo4.webp",
+"assets/products/shu7663tuo5.webp"
+],
+"origin": "Юнань, Китай",
+"description": "Шу Пуер, приготовлений за багатолітнім рецептом, котрий вражає поціновачів вже понад 50 років. Має класичний пуерний смак, з земельними, древесними та глибокими нотами.",
+"on_order": false,
+"badge": "NEW",
+"brewing": {
+"steeps": 0,
+"time": 20,
+"grams": 0
+}
+},
+{
+"id": 1779932202777,
+"name": "Шу Пуер \"Кирпич\"",
+"category": "Puerh",
+"price": 400,
+"variants": {
+"50": 250,
+"100": 400,
+"250": 1000
+},
+"image": "assets/products/Shubricktuo2.webp",
+"images": [
+"assets/products/Shubricktuo2.webp",
+"assets/products/Shubricktuo1.webp"
+],
+"origin": "Юнань, Китай",
+"description": "Пресований в брикет Шу пуер від легендарної фабрики Xiaguan.",
+"on_order": false,
+"badge": "NEW",
+"brewing": {
+"steeps": 0,
+"time": 20,
+"grams": 0
+}
+},
+{
+"id": 1776520702809,
+"name": "Фен Хуан Дан Цун",
+"category": "Oolong",
+"price": 600,
+"variants": {
+"50": 300,
+"100": 600
+},
+"image": "assets/products/fxdc.webp",
+"images": [
+"assets/products/fxdc.webp"
+],
+"origin": "Гори \"Фен Хуан\", провінція Гуандун",
+"description": "Легендарний гуандунський улун, що славиться своїм феноменальним ароматом, який порівнюють з пахощами диких орхідей та меду. Це справжній «чайний парфум», який став візитівкою гір Фенікса завдяки своєму яскравому багатогранному смаку та сильному енергійному стану.",
+"on_order": false,
+"badge": "HIT",
+"brewing": {
+"steeps": 7,
+"time": 20,
+"grams": 8
+}
+},
+{
+"id": 1775867761283,
+"name": "Габа \"Сапфір\"",
+"category": "Gaba",
+"price": 500,
+"variants": {
+"50": 300,
+"100": 500
+},
+"image": "assets/products/Gabasaphire.webp",
+"images": [
+"assets/products/Gabasaphire.webp"
+],
+"origin": "Китай, провінція Юньнань",
+"description": "Вишуканий Габа-улун з вулканічних високогір’їв Тенчуна (провінція Юньнань). Завдяки багатим на мінерали ґрунтам, чай має глибокий, чистий смак з нотами ягідного джему та ледь помітною прохолодою м’яти. Він м'яко занурює у стан глибокого спокою, наче очищуючи думки від зайвого шуму.",
+"on_order": false,
+"badge": "HIT",
+"brewing": {
+"steeps": 8,
+"time": 20,
+"grams": 8
+}
+},
+{
+"id": 1776520434777,
+"name": "Шу Пуер \"Точа\"",
+"category": "Puerh",
+"price": 500,
+"variants": {
+"50": 300,
+"100": 500
+},
+"image": "assets/products/tuocha2.webp",
+"images": [
+"assets/products/tuocha2.webp",
+"assets/products/toucha1.webp"
+],
+"origin": "Юнань, Китай",
+"description": "Легендарний шу пуер, рецепт котрого створювався для експорту пуеру в францію, але він вийшов таким смачним, що став топовою позицію флагманської фабрики \"Xiaguan\"",
+"on_order": false,
+"badge": "HIT",
+"brewing": {
+"steeps": 7,
+"time": 20,
+"grams": 8
+}
+},
+{
+"id": 1775867846686,
+"name": "Да Хун Пао \"Вогняний\"",
+"category": "Oolong",
+"price": 400,
+"variants": {
+"50": 250,
+"100": 400
+},
+"image": "assets/products/DHP.webp",
+"images": [
+"assets/products/DHP.webp"
+],
+"origin": "Китай, північ провінції Фуцзянь",
+"description": "Еталонний скельний улун з північної Фуцзяні, що втілює тисячолітню чайну традицію гір Уї. У кожній піалі розкривається міцний характер: ноти підсмаженого хліба, стиглих фруктів та легкий серпанок деревного вугілля. Чай, що зігріває зсередини, дарує ясність думок та непохитний внутрішній спокій.",
+"on_order": false,
+"badge": "HIT",
+"brewing": {
+"steeps": 7,
+"time": 20,
+"grams": 8
+}
+},
+{
 "id": 1779654407869,
 "name": "Шу Пуер \"Точа 23\"",
 "category": "Puerh",
@@ -28,47 +278,24 @@ var products = [
 }
 },
 {
-"id": 1775867846686,
-"name": "Да Хун Пао \"Вогняний\"",
+"id": 1775867941281,
+"name": "Женьшень Улун",
 "category": "Oolong",
-"price": 400,
+"price": 350,
 "variants": {
-"50": 250,
-"100": 400
+"50": 200,
+"100": 350
 },
-"image": "assets/products/DHP.webp",
+"image": "assets/products/Ginseng.webp",
 "images": [
-"assets/products/DHP.webp"
+"assets/products/Ginseng.webp"
 ],
-"origin": "Китай, північ провінції Фуцзянь",
-"description": "Еталонний скельний улун з північної Фуцзяні, що втілює тисячолітню чайну традицію гір Уї. У кожній піалі розкривається міцний характер: ноти підсмаженого хліба, стиглих фруктів та легкий серпанок деревного вугілля. Чай, що зігріває зсередини, дарує ясність думок та непохитний внутрішній спокій.",
+"origin": "Китай, провінція Фуцзянь",
+"description": "Традиційний улун у формі щільно пресованих гранул, насичених екстрактом женьшеню. При заварюванні «камінці» повільно розкриваються, віддаючи напою пряність та густину. Це чай з характером, який окутує теплом та залишає в роті приємну солодкість, що тримається годинами.",
 "on_order": false,
-"badge": "NEW",
+"badge": "none",
 "brewing": {
-"steeps": 7,
-"time": 20,
-"grams": 8
-}
-},
-{
-"id": 1775867761283,
-"name": "Габа \"Сапфір\"",
-"category": "Gaba",
-"price": 500,
-"variants": {
-"50": 300,
-"100": 500
-},
-"image": "assets/products/Gabasaphire.webp",
-"images": [
-"assets/products/Gabasaphire.webp"
-],
-"origin": "Китай, провінція Юньнань",
-"description": "Вишуканий Габа-улун з вулканічних високогір’їв Тенчуна (провінція Юньнань). Завдяки багатим на мінерали ґрунтам, чай має глибокий, чистий смак з нотами ягідного джему та ледь помітною прохолодою м’яти. Він м'яко занурює у стан глибокого спокою, наче очищуючи думки від зайвого шуму.",
-"on_order": false,
-"badge": "NEW",
-"brewing": {
-"steeps": 8,
+"steeps": 6,
 "time": 20,
 "grams": 8
 }
@@ -89,7 +316,7 @@ var products = [
 "origin": "Китай, південь провінції Фуцзянь",
 "description": "Світлий улун з південної Фуцзяні в його найбільш природному, «фермерському» вигляді. Наявність чайних черешків дарує настою особливу медову солодкість та густину, якої немає в рафінованих сортах. Смак свіжої весняної зелені з ароматом бузку та довгим, соковитим післясмаком.",
 "on_order": false,
-"badge": "NEW",
+"badge": "none",
 "brewing": {
 "steeps": 6,
 "time": 20,
@@ -113,57 +340,11 @@ var products = [
 "origin": "Гори Уї (Уїшань)",
 "description": "Цей сорт став еталоном скельних улунів завдяки глибокому, багатогранному смаку з м’якими деревними та медовими нотами, а також сильному заспокійливому ефекту.",
 "on_order": false,
-"badge": "NEW",
+"badge": "none",
 "brewing": {
 "steeps": 7,
 "time": 20,
 "grams": 7
-}
-},
-{
-"id": 1776520702809,
-"name": "Фен Хуан Дан Цун",
-"category": "Oolong",
-"price": 600,
-"variants": {
-"50": 300,
-"100": 600
-},
-"image": "assets/products/fxdc.webp",
-"images": [
-"assets/products/fxdc.webp"
-],
-"origin": "Гори \"Фен Хуан\", провінція Гуандун",
-"description": "Легендарний гуандунський улун, що славиться своїм феноменальним ароматом, який порівнюють з пахощами диких орхідей та меду. Це справжній «чайний парфум», який став візитівкою гір Фенікса завдяки своєму яскравому багатогранному смаку та сильному енергійному стану.",
-"on_order": false,
-"badge": "NEW",
-"brewing": {
-"steeps": 7,
-"time": 20,
-"grams": 8
-}
-},
-{
-"id": 1775867941281,
-"name": "Женьшень Улун",
-"category": "Oolong",
-"price": 350,
-"variants": {
-"50": 200,
-"100": 350
-},
-"image": "assets/products/Ginseng.webp",
-"images": [
-"assets/products/Ginseng.webp"
-],
-"origin": "Китай, провінція Фуцзянь",
-"description": "Традиційний улун у формі щільно пресованих гранул, насичених екстрактом женьшеню. При заварюванні «камінці» повільно розкриваються, віддаючи напою пряність та густину. Це чай з характером, який окутує теплом та залишає в роті приємну солодкість, що тримається годинами.",
-"on_order": false,
-"badge": "NEW",
-"brewing": {
-"steeps": 6,
-"time": 20,
-"grams": 8
 }
 },
 {
@@ -182,55 +363,11 @@ var products = [
 "origin": "Китай",
 "description": "Ніжний чай з високогір’я, що поєднує свіжість зеленого листа та м’якість вершків. Смак збалансований, без терпкості, з приємною природною солодкістю. Гарний вибір, щоб познайомитися з китайською традицією та відновити сили протягом дня.",
 "on_order": false,
-"badge": "NEW",
+"badge": "HIT",
 "brewing": {
 "steeps": 5,
 "time": 20,
 "grams": 7
-}
-},
-{
-"id": 1776520434777,
-"name": "Шу Пуер \"Точа\"",
-"category": "Puerh",
-"price": 500,
-"variants": null,
-"image": "assets/products/tuocha2.webp",
-"images": [
-"assets/products/tuocha2.webp",
-"assets/products/toucha1.webp"
-],
-"origin": "Юнань, Китай",
-"description": "Легендарний шу пуер, рецепт котрого створювався для експорту пуеру в францію, але він вийшов таким смачним, що став топовою позицію флагманської фабрики \"Xiaguan\"",
-"on_order": false,
-"badge": "NEW",
-"brewing": {
-"steeps": 7,
-"time": 20,
-"grams": 8
-}
-},
-{
-"id": 1775868050998,
-"name": "Уі Шуй Сянь \"Нарцисс\"",
-"category": "Oolong",
-"price": 375,
-"variants": {
-"50": 225,
-"100": 375
-},
-"image": "assets/products/Narciss.webp",
-"images": [
-"assets/products/Narciss.webp"
-],
-"origin": "Китай, північ провінції Фуцзянь",
-"description": "Традиційний темний улун з північної Фуцзяні, що росте на багатих мінералами скелях. Має насичений, густий настій з відтінками вологої деревини, меду та весняних квітів. Нарцис вирізняється особливою м’якістю та «об'ємом» у роті, що робить кожне ковтання справжньою церемонією.",
-"on_order": false,
-"badge": "NEW",
-"brewing": {
-"steeps": 7,
-"time": 20,
-"grams": 8
 }
 },
 {
@@ -254,6 +391,29 @@ var products = [
 "steeps": 7,
 "time": 20,
 "grams": 7
+}
+},
+{
+"id": 1775868050998,
+"name": "Уі Шуй Сянь \"Нарцисс\"",
+"category": "Oolong",
+"price": 375,
+"variants": {
+"50": 225,
+"100": 375
+},
+"image": "assets/products/Narciss.webp",
+"images": [
+"assets/products/Narciss.webp"
+],
+"origin": "Китай, північ провінції Фуцзянь",
+"description": "Традиційний темний улун з північної Фуцзяні, що росте на багатих мінералами скелях. Має насичений, густий настій з відтінками вологої деревини, меду та весняних квітів. Нарцис вирізняється особливою м’якістю та «об'ємом» у роті, що робить кожне ковтання справжньою церемонією.",
+"on_order": false,
+"badge": "none",
+"brewing": {
+"steeps": 7,
+"time": 20,
+"grams": 8
 }
 },
 {
@@ -316,79 +476,11 @@ var products = [
 "origin": "Уїшань, Китай",
 "description": "Легендарний улун із скельним характером та глибоким ароматом. Має дуже яскравий, насичений смак із нотами пряності, з відтінками деревини, смажених горіхів і печеного каштана, та довгим солодкуватим післясмаком.\n\nЧай для тонусу, концентрації та яскравих смакових відчуттів.",
 "on_order": false,
-"badge": "none",
+"badge": "SALE",
 "brewing": {
 "steeps": 6,
 "time": 20,
 "grams": 8
-}
-},
-{
-"id": 5,
-"name": "Пуер \"Менхай\"",
-"category": "Puerh",
-"price": 275,
-"variants": {
-"50": 150,
-"100": 275,
-"357": 500
-},
-"image": "assets/products/prod_5_0.webp",
-"images": [
-"assets/products/prod_5_0.webp",
-"assets/products/prod_5_1.webp",
-"assets/products/prod_5_2.webp"
-],
-"origin": "Юньнань, Китай",
-"description": "Класичний витриманий шу пуер у формі млинця. Не надто гіркий, із землянистими нотами, деревними відтінками та легкою солодкістю.\n\nЧай для тепла, спокою та м’якої концентрації.",
-"on_order": true,
-"badge": "none",
-"brewing": {
-"steeps": 6,
-"time": 20,
-"grams": 8
-}
-},
-{
-"id": 1775867642449,
-"name": "Габа Да Хун Пао",
-"category": "Gaba",
-"price": 0,
-"variants": null,
-"image": "assets/products/GabaDPH2.webp",
-"images": [
-"assets/products/GabaDPH2.webp",
-"assets/products/GabaDHP1.webp"
-],
-"origin": "Китай, провінція Фуцзянь, гори Уї",
-"description": "Унікальне поєднання скельного характеру Да Хун Пао та заспокійливої дії Габа-чаю. У смаку відчуваються пряні нотки вогню, печеної корочки хліба та легка ягідна кислинка. Цей чай м’яко \"перезавантажує\" нервову систему, знімаючи напругу після складного дня, при цьому залишаючи розум ясним.",
-"on_order": true,
-"badge": "none",
-"brewing": {
-"steeps": 5,
-"time": 20,
-"grams": 8
-}
-},
-{
-"id": 2,
-"name": "Червоний Дракон Пуер",
-"category": "Puerh",
-"price": 0,
-"variants": null,
-"image": "assets/products/prod_2_0.webp",
-"images": [
-"assets/products/prod_2_0.webp",
-"assets/products/prod_2_1.webp"
-],
-"origin": "Юньнань, Китай",
-"description": "Класичний шу без різкої гіркоти. Ноти вологого лісу, темного дерева та легкої горіхової ноти.\n\nЧай для бадьорості, світлості думок та більшої концентрації.",
-"on_order": true,
-"badge": "none",
-"brewing": {
-"steeps": 6,
-"time": 20,
-"grams": 7
 }
 },
 {
@@ -413,24 +505,29 @@ var products = [
 }
 },
 {
-"id": 4,
-"name": "Ті Гуань Інь (Пачки)",
-"category": "Oolong",
-"price": 0,
-"variants": null,
-"image": "assets/products/prod_4_0.webp",
+"id": 5,
+"name": "Пуер \"Менхай\"",
+"category": "Puerh",
+"price": 275,
+"variants": {
+"50": 150,
+"100": 275,
+"357": 500
+},
+"image": "assets/products/prod_5_0.webp",
 "images": [
-"assets/products/prod_4_0.webp",
-"assets/products/prod_4_1.webp"
+"assets/products/prod_5_0.webp",
+"assets/products/prod_5_1.webp",
+"assets/products/prod_5_2.webp"
 ],
-"origin": "Фуцзянь, Китай",
-"description": "Високогірний улун із квітковим ароматом, м’якою вершковістю та солодким післясмаком. Красиве скручене листя розкривається у великі пластини.\n\nЧай для легкості, гармонії та приємного релаксу.",
-"on_order": true,
-"badge": "none",
+"origin": "Юньнань, Китай",
+"description": "Класичний витриманий шу пуер у формі млинця. Не надто гіркий, із землянистими нотами, деревними відтінками та легкою солодкістю.\n\nЧай для тепла, спокою та м’якої концентрації.",
+"on_order": false,
+"badge": "SALE",
 "brewing": {
-"steeps": 5,
+"steeps": 6,
 "time": 20,
-"grams": 7
+"grams": 8
 }
 }
 ];
